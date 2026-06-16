2b6e9b075ba9c9f1d9effcfe074a459e6fd35d26
sreeparvathy-ms <chn25eca215@ceconline.edu>
2026-06-16 15:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,6 +102,9 @@ else:
             st.caption(
                 current_question["section"]
             )
+            st.caption(
+    "1 = Strongly Disagree | 2 = Disagree | 3 = Neutral | 4 = Agree | 5 = Strongly Agree"
+)
 
             response = st.radio(
                 current_question["question"],
